f780a838c547da5a4110675089e6b1042bac7079
cihanb <cihangirb@hotmail.com>
2017-01-28 15:22:59 -0800

    test code for windows python connectivity and performance with redislabs

---
+++ run-ram.bat
@@ -0,0 +1,3 @@
+start python test.py 1 10.0.0.52 19960
+start python test.py 2 10.0.0.53 19960
+start python test.py 3 10.0.0.54 19960
---
+++ run-rof.bat
@@ -0,0 +1,4 @@
+start python test.py 1 10.0.0.44 11311
+start python test.py 2 10.0.0.45 11311
+start python test.py 3 10.0.0.54 11311
+start python test.py 4 10.0.0.53 11311
